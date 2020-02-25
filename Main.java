2bbdabf435cffff9dc5a331366746fbe95623618
Hiro-mn <54801478+Hiro-mn@users.noreply.github.com>
2020-02-25 23:38:13 +0900

    練習です

--- Main.java
+++ Main.java
@@ -4,6 +4,14 @@ public class Main {
     お試しです
     練習です
     */
+    // 文字　シングルクォーテーションで囲む
+    char a = 'a';
+    //整数　byte short int long longだけ数字の後ろにLをつける
+    long y = 77777777L;
+    //少数点 float double floatだけ数字の後ろにFをつける
+    float f = 77.77F;
+
+
     public static void main(String[] args) {
         System.out.println("こんにちは");
     }
