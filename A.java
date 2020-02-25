1c05901aac54a20d9333380f0d3ff065ae1a9140
Hiro-mn <54801478+Hiro-mn@users.noreply.github.com>
2020-02-25 23:51:38 +0900

    練習その２です

---
+++ A.java
@@ -0,0 +1,14 @@
+public class A {
+    public static void main (String[] args) {
+        int i = 10 / 3;
+        System.out.println(i);
+        int a = 10 % 3;
+        System.out.println(a);
+        int x = 5;
+        x = x + 12;
+        x += 12;
+        System.out.println(x);
+    }
+    
+    
+}
