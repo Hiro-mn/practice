d44abe4c4621087a524c45828a35f863f415acac
Hiro-mn <54801478+Hiro-mn@users.noreply.github.com>
2020-03-04 00:05:30 +0900

    練習４です

--- D.java
+++ D.java
@@ -2,12 +2,16 @@ public class D {
     public static void main(String[] args) {
         //if
         int score = 85;
-        if (score > 80) {
-         System.out.println("great");
-        } else if (score > 60) {
-            System.out.println("soso");
-        } else {
-            System.out.println("bad");
-        }
+       // if (score > 80) {
+         //System.out.println("great");
+        //} else if (score > 60) {
+           //System.out.println("soso");
+       // } else {
+            //System.out.println("bad");
+        //}
+        //もうひとつの書き方もある。下記に記載。
+        //返り値 = 条件 ? 条件が真だったときの値 : 条件が偽だったときの値
+        String msg = score > 80 ? "great" : "soso";
+        System.out.println(msg);
     }
 }
