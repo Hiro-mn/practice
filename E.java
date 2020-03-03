13ec89d6c80d12415bb41549452516a775ef489f
Hiro-mn <54801478+Hiro-mn@users.noreply.github.com>
2020-03-04 00:53:57 +0900

    練習５です

---
+++ E.java
@@ -0,0 +1,21 @@
+public class E {
+    public static void main(String[] args){
+      //switch
+      String signal = "pink";
+       switch (signal) {
+           case "red":
+            System.out.println("stop");
+            break;
+            case "blue":
+            System.out.println("go");
+            break;
+            case "yellow":
+            System.out.println("zyokou");
+            break;
+            default:
+            System.out.println("no");
+            break;
+
+       }
+    }
+}
