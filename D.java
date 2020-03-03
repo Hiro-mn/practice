3ddab950a0977a879265f0302238e694d49b750f
Hiro-mn <54801478+Hiro-mn@users.noreply.github.com>
2020-03-03 23:51:19 +0900

    練習４です

---
+++ D.java
@@ -0,0 +1,13 @@
+public class D {
+    public static void main(String[] args) {
+        //if
+        int score = 85;
+        if (score > 80) {
+         System.out.println("great");
+        } else if (score > 60) {
+            System.out.println("soso");
+        } else {
+            System.out.println("bad");
+        }
+    }
+}
