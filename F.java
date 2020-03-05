58e215d6b35fe6fc5fe5922f4afaee62fbe0b92a
Hiro-mn <54801478+Hiro-mn@users.noreply.github.com>
2020-03-06 01:42:13 +0900

    練習７です

---
+++ F.java
@@ -0,0 +1,17 @@
+public class F {
+    public static void main(String[] args) {
+        //for
+        //0-9の表示
+        //for (int i = 0; i < 10; i++) {
+            //System.out.println(i);
+            for (int i = 0; i < 10; i++) {
+                if (i == 5) {
+                    //continue;
+                    break;
+                }
+                    System.out.println(i);
+                
+            }
+        //}
+    }
+}
