f540d36bfa28ff6c6fbc527c924ab144caf2a9da
Hiro-mn <54801478+Hiro-mn@users.noreply.github.com>
2020-03-05 22:16:57 +0900

    練習６です

---
+++ B.java
@@ -0,0 +1,14 @@
+public class B {
+    public static void main(String[] args) {
+        //while
+        int i = 100;
+        //while (i < 11) {
+            //System.out.println(i);
+            //i++;
+        //}
+          do {
+             System.out.println(i);
+             i++; 
+          } while (i < 11);
+    }
+}
